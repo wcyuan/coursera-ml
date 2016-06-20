1157eb79ae3bcbf106eca117604e4f67ac9a72c0
Conan Yuan <conanyuan+cloud9@gmail.com>
2016-06-20 17:28:43 +0000

    finish gradientDescent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    err = X * theta - y;
 
 
-
-
-
-
+    theta = theta - alpha / m * (sum(repmat(err, 1, size(X, 2)) .* X)');
 
     % ============================================================
 
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,7 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
+    err = X * theta - y;
+    theta = theta - alpha / m * (sum(err .* X)');
 
 
 
