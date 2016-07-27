deff1e15392f3fe86444a662aaaf6179f58c9140
Conan Yuan <conanyuan+cloud9@gmail.com>
2016-07-27 13:49:11 +0000

    ex7: findClosestCentroids

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,11 +21,10 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
-
+for i = 1:size(X,1)
+    [v, ix] = min(sum((repmat(X(i,:),K,1) .- centroids) .^ 2, 2));
+    idx(i) = ix;
+end
 
 % =============================================================
 
